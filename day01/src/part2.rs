756f6ac7168955750144697556538ca3641f131c
matason <chrispmaiden@gmail.com>
2024-01-07 19:26:38 +0000

    Butchering day 1, part 2

--- day01/src/part2.rs
+++ day01/src/part2.rs
@@ -1,3 +1,25 @@
+use std::fs;
+use itertools::Itertools;
+use itertools::FoldWhile::{Continue, Done};
+
 fn main() {
-    println!("Hello, world!");
+    let input = fs::read_to_string("./day01-input.txt").expect("Should be able to read the file");
+    println!("{}", run(input))
+}
+
+fn run(input: String) -> i32 {
+    return input.chars().enumerate().fold_while(0, |mut acc, c| {
+        if '(' == c.1 { acc += 1 } else if ')' == c.1 { acc -= 1 } else { acc += 0 }
+        if acc < 0 { Done(c.0 as i32 + 1) } else { Continue(acc) }
+    }).into_inner();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_day01() {
+        assert_eq!(5, run(String::from("()())")));
+    }
 }
