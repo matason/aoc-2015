796caa6128489c6aa7c292a1e7cb6a430f251747
matason <chrispmaiden@gmail.com>
2024-01-10 12:30:35 +0000

    Day 2, part 1

--- day02/src/bin/part1.rs
+++ day02/src/bin/part1.rs
@@ -1,12 +1,25 @@
 use std::fs;
 
 fn main() {
-    let input = fs::read_to_string("./day02-input.txt").expect("Should be able to read the file");
+    let input: String = fs::read_to_string("./day02-input.txt").expect("Should be able to read the file");
    println!("{}", run(input))
 }
 
 fn run(input: String) -> i32 {
-    101
+    input
+        .lines()
+        .map(|line| {
+            let mut d: Vec<_> = line
+                .split('x')
+                .map(|value| {
+                    value.parse::<i32>().expect("Should be a parseable i32")
+                })
+                .collect();
+            d.sort();
+
+            (d[0] * d[1]) + (2 * (d[0] * d[1])) + (2 * (d[1] * d[2])) + (2 * (d[0] * d[2]))
+        })
+        .sum()
 }
 
 #[cfg(test)]
